cbc36c71e5c7e983668f5c91ecd2f3b24d7c11d7
awqsomee <awqsome@bk.ru>
2022-12-03 05:58:05 +0300

    Update corsMiddleware.js

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
